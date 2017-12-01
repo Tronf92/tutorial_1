c2d1188ad87594f93ec71eba2ed8666998518161
na <ady.niko51@gmail.com>
2017-12-01 19:17:27 +0200

    first commit

---
+++ tutorial 1/Source.cpp
@@ -0,0 +1,7 @@
+#include <Windows.h>
+using namespace std;
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow){
+	MessageBox(NULL, "Goodbye, curel world!", "Note",MB_OK);
+	return 0;
+}
